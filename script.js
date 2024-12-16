4c9474d22effc6640efa8da8ba0932ea6ec8103b
Abtolo <102583032+Abtolo@users.noreply.github.com>
2024-12-16 10:05:51 +0100

    Assigning classes
    Assigned classes and put a checkbox now just need to adjust the lyout of the to do list elements

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 "use strict";
 
 const theButton = document.querySelector(".btn");
-const input = document.querySelector(".theTask");
+const input = document.querySelector(".theInput");
 
 theButton.addEventListener("click", function () {
-  let tasks = document.querySelector(".thetasks");
-  let createTask = document.createElement("li");
-  let createButton = document.createElement("button");
-  createButton.textContent = "❌";
+  if (input.value === "") {
+    alert("You didn't  write a task");
+  } else {
+    let tasks = document.querySelector(".thetasks");
+    let createTask = document.createElement("li");
+    let createButton = document.createElement("button");
+    let createCheckbox = document.createElement("input");
 
-  createTask.textContent = input.value;
+    createCheckbox.setAttribute("type", "checkbox");
+    createButton.setAttribute("class", "new-btn");
+    createTask.setAttribute("type", "checkbox");
+    createTask.setAttribute("class", "new-task");
+    createButton.textContent = "❌";
 
-  tasks.appendChild(createTask);
-  tasks.appendChild(createButton);
-  input.value = "";
+    createTask.textContent = input.value;
+
+    tasks.appendChild(createCheckbox);
+    tasks.appendChild(createTask);
+    tasks.appendChild(createButton);
+    input.value = "";
+  }
 });
