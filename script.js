32202b4fb0c1130e84d722280baecf6948a89d5e
Abtolo <102583032+Abtolo@users.noreply.github.com>
2024-12-19 02:03:30 +0100

    Trying
    testing different ways to make it so that I can  remove a task at timz

--- script.js
+++ script.js
@@ -2,27 +2,35 @@
 
 const theButton = document.querySelector(".btn");
 const input = document.querySelector(".theInput");
+const removebtn = document.querySelector(".new-btn");
 
 theButton.addEventListener("click", function () {
   if (input.value === "") {
     alert("You didn't  write a task");
   } else {
+    let theDiv = document.createElement("div");
     let tasks = document.querySelector(".thetasks");
     let createTask = document.createElement("li");
     let createButton = document.createElement("button");
     let createCheckbox = document.createElement("input");
 
+    theDiv.setAttribute("class", "taskDiv");
     createCheckbox.setAttribute("type", "checkbox");
     createButton.setAttribute("class", "new-btn");
+    createButton.textContent = "❌";
     createTask.setAttribute("type", "checkbox");
     createTask.setAttribute("class", "new-task");
-    createButton.textContent = "❌";
 
     createTask.textContent = input.value;
 
-    tasks.appendChild(createCheckbox);
-    tasks.appendChild(createTask);
-    tasks.appendChild(createButton);
+    theDiv.appendChild(createCheckbox);
+    theDiv.appendChild(createTask);
+    theDiv.appendChild(createButton);
+    tasks.appendChild(theDiv);
     input.value = "";
   }
+});
+
+removebtn.addEventListener("click", function () {
+  document.querySelector(".taskDiv").classList.add(".display");
 });
